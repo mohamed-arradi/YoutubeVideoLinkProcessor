fb270ef628110f64cf0aa9b20aa4ea440efe0715
Mohamed Arradi-Alaoui( اراذي حال و ي محمد) <arradi.mohamed@gmail.com>
2017-01-23 13:04:03 +0000

    update example demo app

--- Example/YoutubeVideoDownloader/ViewController.swift
+++ Example/YoutubeVideoDownloader/ViewController.swift
@@ -11,19 +11,41 @@ import YoutubeVideoDownloader
 
 class ViewController : UIViewController {
     
-    @IBOutlet weak var webView : UIWebView!
+    @IBOutlet weak var textView : UITextView!
+    @IBOutlet weak var textField : UITextField!
+    @IBOutlet weak var button : UIButton!
+    @IBOutlet weak var activityLoader : UIActivityIndicatorView!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        activityLoader.isHidden = true
+        textField.text = "https://www.youtube.com/watch?v=X7FVYJ6ONXk"
+    }
+
+    @IBAction func getVideoLink(sender : UIButton)
+    {
+        self.view.endEditing(true)
         
-        let youtubeManager : YoutubeVideoManager
+        guard self.textField.text!.isEmpty == false else {
+            let alert = UIAlertController(title: "Alert", message: "Link Empty", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
         
-         youtubeManager = YoutubeVideoManager(url: "https://www.youtube.com/watch?v=RDTxeY7akNA")
-         
-         youtubeManager.fetchVideoLink(completion: { (link : String, error : NSError?) in
-        self.webView.loadRequest(URLRequest(url: URL(string: link)!))
+        activityLoader.startAnimating()
+        activityLoader.isHidden = false
+        
+        let youtubeManager : YoutubeVideoManager = YoutubeVideoManager(url: self.textField.text!)
+        
+        youtubeManager.fetchVideoLink(completion: { (link : String, error : NSError?) in
             
-         })
+            DispatchQueue.main.sync {
+               self.textView.text = link
+               self.activityLoader.isHidden = true
+               self.activityLoader.stopAnimating()
+            }
+            
+        })
     }
 }
